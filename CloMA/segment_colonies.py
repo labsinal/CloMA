48720f30bbbe241dc51ba6b9961392cd535a317d
fred25 <fredericomattos25@gmail.com>
2025-10-21 16:26:54 -0300

    add incomplete segmentation

---
+++ CloMA/segment_colonies.py
@@ -0,0 +1,124 @@
+"""
+Module that segment colonies from well images
+"""
+
+######################################
+# imports
+from numpy import ndarray, copy, ogrid, where, ones, zeros
+from cv2 import imread, imwrite
+from cv2 import createCLAHE
+from cv2 import cvtColor, COLOR_BGR2GRAY
+from skimage.morphology import reconstruction
+from skimage.filters import threshold_otsu
+from skimage.feature import peak_local_max
+from skimage.segmentation import watershed
+from scipy.ndimage import distance_transform_edt, label
+from os.path import join, basename, dirname 
+
+######################################
+# Define helper functions
+
+def segment_well_colonies(image:ndarray) -> ndarray:
+    """
+    Function that segments well colonies photo
+
+    Args:
+        image:ndarray | well image as numpy array
+    Returns
+    ndarray | segmented well image
+    """
+
+    # Create CLAHE filter instance
+    clahe = createCLAHE(clipLimit=2.0, tileGridSize=(10, 10))
+
+    # apply CLAHE to image
+    cl1 = clahe.apply(cvtColor(image, COLOR_BGR2GRAY))
+
+    # Create dilation parameters
+    seed = copy(-cl1)
+    seed[1:-1, 1:-1] = (-cl1).min()
+    mask = -cl1
+
+    # Execute dilation reconstruction
+    dilated = reconstruction(seed, mask, method='dilation')
+
+    # Create colonies image
+    colonies = -cl1 - dilated
+
+    # Create circle mas
+    rows, cols = colonies.shape
+    cy, cx = rows // 2, cols // 2
+    Y, X = ogrid[:rows, :cols]
+    circle_mask = (X - cx)**2 + (Y - cy)**2 <= (550 * 0.8)**2
+
+    # Mask segmentation
+    masked_colonies = where(circle_mask, colonies, 0)
+
+    # Binarize segmentation
+    thresh = threshold_otsu(masked_colonies)
+    binary = masked_colonies > thresh
+
+    # Calculate euclidean distance transform
+    distance = distance_transform_edt(binary)
+
+    # find local max to watershed
+    coords = peak_local_max(distance, footprint=ones((3, 3)), labels=binary)
+    mask = zeros(distance.shape, dtype=bool)
+    mask[tuple(coords.T)] = True
+    markers, _ = label(mask)
+    
+    # Run watershed
+    labels = watershed(-distance, markers, mask=binary)
+
+    return labels
+
+######################################
+# Define main function
+
+def main() -> None:
+    """
+    Code's main function
+    """
+    # import argument parsing library
+    from argparse import ArgumentParser
+
+    # create parser instance
+    parser = ArgumentParser(description="")
+
+    # add arguments to parser
+    parser.add_argument("-i", "--input_image",
+                        action="store",
+                        required=True,
+                        dest="input_path",
+                        help="Path to image to be segmented")
+    
+    parser.add_argument("-o", "--output_image",
+                        action="store",
+                        required=False,
+                        default=None,
+                        dest="output_path",
+                        help="Path to save segmentation")
+    
+    args = parser.parse_args()
+
+    # open image
+    image = imread(args.input_path)
+
+    # apply segmentation algorithm
+    segmentation = segment_well_colonies(image)
+
+    # save segmentation
+    if args.output_path:
+        filename = args.output_path
+    else:
+       filename = join(dirname(args.input_path), f"seg_{basename(args.input_path).split(".")[0]}.tif") 
+
+    print(f"Saving segmentation as {filename}")
+    imwrite(filename, segmentation)
+
+######################################
+# Call main function id runned directly
+if __name__ == "__main__":
+    main()
+
+# end of current module
